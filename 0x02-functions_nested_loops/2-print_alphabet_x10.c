454f63c733732c95a16ac5978d847198fe3a2d77
SaraGhanem <saraghanem2002@gmail.com>
2023-04-26 14:12:43 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
  * Return : void
  */
 void print_alphabet_x10(void)
@@ -10,17 +10,16 @@ void print_alphabet_x10(void)
 char x;
 int i;
 
-
-
 for (i = 0; i < 10; i++)
 {
 x = 'a';
+
 while (x <= 'z')
 {
-_putchar(x);
-			
+_putchar(x);			
 x++;
 }
+
 _putchar('\n');
 }
 }
