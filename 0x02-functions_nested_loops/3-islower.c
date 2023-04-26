b72017ec3cbfc4aa17e5d708415cdf98427bb437
SaraGhanem <saraghanem2002@gmail.com>
2023-04-26 14:42:12 -0700

    push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower -  checks for lowercase character.
+ * return 1 for lowercase or 0 otherwise.
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
