e903e6b91204dcf7ac25b192defa14f270297de4
SaraGhanem <saraghanem2002@gmail.com>
2023-05-03 19:10:32 -0700

    push

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first integer to swap
+ * @b: seconf integer to swap
+ * Return : void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
