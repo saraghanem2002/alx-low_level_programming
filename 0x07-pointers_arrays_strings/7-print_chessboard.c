2011cfc938ddbc068b50c1f55682c253ba879fba
SaraGhanem <saraghanem2002@gmail.com>
2023-05-09 18:25:10 -0700

    push

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
