790ec875d8bf10ed76c3128c911444e60810e7e6
SaraGhanem <saraghanem2002@gmail.com>
2023-05-18 16:34:22 -0700

    push

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,10 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes
- * return : pointer to the allocated memory
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
